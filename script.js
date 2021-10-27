df1779f7a2d55f98c022ea785375a5df448bef75
trujilml <mickeytrj@gmail.com>
2021-10-26 20:40:43 -0500

    established and figuring out api of openstreetmap

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //open trip link https://opentripmap.com/en/#17.25/30.28881/-97.74212 
-//will implement api key tomorrow
+
+//put cdn of jquery
+var apiKey = "5ae2e3f221c38a28845f05b6e1bed42d25cd90333367bf8a6e23fe6f";
+
+var austinTX = "https://www.openstreetmap.org/node/1801308037";
+
+var mapOfAustin = "https://www.openstreetmap.org/#map=18/30.27113/-97.74464";
+
+var mapOfAustinTacoPlaces = "http://api.opentripmap.com/0.1/en/places/bbox?" ;
+
+//how to break it down
+
+// lon_min=38.364285
+// &lat_min=59.855685
+// &lon_max=38.372809
+// &lat_max=59.859052
+// &kinds=churches
+// &format=geojson
+// &apikey=xxxxx"
+
+
+
+var searchCity = $(".btn"); 
