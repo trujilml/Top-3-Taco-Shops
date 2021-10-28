034ce3d58e8c028bbdf7d603ac2c35b8df0318d2
trujilml <mickeytrj@gmail.com>
2021-10-27 20:20:37 -0500

    seperate js file

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-//openweather one call api - will declare austin tx weather, icon and percipitation level (if desired)
-//https://openweathermap.org/api/one-call-api
-
-
-
-
-var apiKey = "";
 
---
+++ weatherpart.js
@@ -0,0 +1,7 @@
+//openweather one call api - will declare austin tx weather, icon and percipitation level (if desired)
+//https://openweathermap.org/api/one-call-api
+
+
+
+
+var apiKey = "";
