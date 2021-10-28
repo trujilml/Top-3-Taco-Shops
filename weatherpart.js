9efb7d99e7d644253c76972009996ad473cdbbf4
trujilml <mickeytrj@gmail.com>
2021-10-27 20:57:24 -0500

    added function jquery

--- weatherpart.js
+++ weatherpart.js
@@ -2,6 +2,27 @@
 //https://openweathermap.org/api/one-call-api
 
 
+var apiKey = "a89f9cf766ff1ad95290ed78ef14f7bf";
 
 
-var apiKey = "";
+var austinForecastUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "Austin" + "&appid=" + apiKey + "&units=imperial";
+
+
+$.ajax({
+    url:  austinForecastUrl,
+    method: "GET"
+}).then(function (response) {
+
+    var displayAustinWeather = $(".weather-element").append("<div>");
+    var currentAustinWeather = displayAustinWeather.append("<p>" + "Austin" + "</p>");
+    displayAustinWeather.append(currentAustinWeather);
+
+    // currentAustinWeather.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+
+    var currentTemperature = currentAustinWeather.append("<p>");
+
+    currentAustinWeather.append(currentTemperature);
+    currentTemperature.append("<p>" + response.main.temp + "\u2109" + "</p>");
+  
+
+});
