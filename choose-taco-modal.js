d1ee2672bb9afc33c9a7ceca38b9379019766968
trujilml <mickeytrj@gmail.com>
2021-10-28 23:05:11 -0500

    created new feature branch for choose taco feature

---
+++ choose-taco-modal.js
@@ -0,0 +1,4 @@
+//will utilize jquery ui, modal and interactive selection menu from the three taco places showing 5 tacos (15 tacos total)
+//refer kevin's pages document on current site looking mockup 
+
+//"what's your favorite taco?" "Choose a taco as a button element"
