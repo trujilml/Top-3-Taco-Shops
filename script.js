5798a4b9ea08aa82717a7ad4c2883491517b56cc
unknown <90293855+fishdestroyer@users.noreply.github.com>
2021-10-30 18:22:45 -0500

    initial map commit

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW5hYm95IiwiYSI6ImNrdjk0dzVmODJhaGwzMHFwNmVhOXYzNTIifQ.hbU5pDSFCqepB13jJ2VATg';
-
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmlzaGRlc3Ryb3llciIsImEiOiJja3ZhaGIwam9hZGNmMnFtYWJoeWY2eDZ5In0.cTey3m_BuUWk9ZIp0n1AhQ';
+//mapboxgl.accessToken = 'pk.eyJ1IjoiZmlzaGRlc3Ryb3llciIsImEiOiJja3ZhOXZrYmowMWF3MnVwMXR2Y2xrZ2hvIn0.upIyfuHDvxKbBv_VGVSwPw';
+//mapbox://styles/fishdestroyer/ckvaf22yi1dyv14teexn4pdq1/draft
+//mapbox://styles/fishdestroyer/ckvaf22yi1dyv14teexn4pdq1
+//below shows to be a const rather than a var- results did not work correctly
 var map = new mapboxgl.Map({
-  container: 'map', // Container ID
-  style: 'mapbox://styles/mapbox/streets-v11',
-  // The map style to use
-  center: [-97.6320655, 30.496208], // Starting position [lng, lat]
-  zoom: 12 // Starting zoom level
+  container: 'map',
+  
+  style: 'mapbox://styles/fishdestroyer/ckvaf22yi1dyv14teexn4pdq1', 
+  center: [-97.700 , 30.255],
+  zoom: 8
+
 });
+
+
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
