8d8db14c98a770b29c58d743d141ed5f7efe0472
unknown <90293855+fishdestroyer@users.noreply.github.com>
2021-10-30 18:29:00 -0500

    update to maps height and width with notations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmlzaGRlc3Ryb3llciIsImEiOiJja3ZhaGIwam9hZGNmMnFtYWJoeWY2eDZ5In0.cTey3m_BuUWk9ZIp0n1AhQ';
 //mapboxgl.accessToken = 'pk.eyJ1IjoiZmlzaGRlc3Ryb3llciIsImEiOiJja3ZhOXZrYmowMWF3MnVwMXR2Y2xrZ2hvIn0.upIyfuHDvxKbBv_VGVSwPw';
-//mapbox://styles/fishdestroyer/ckvaf22yi1dyv14teexn4pdq1/draft
+//mapbox://styles/fishdestroyer/ckvaf22yi1dyv14teexn4pdq1/draft- draft of maps
 //mapbox://styles/fishdestroyer/ckvaf22yi1dyv14teexn4pdq1
 //below shows to be a const rather than a var- results did not work correctly
 var map = new mapboxgl.Map({
