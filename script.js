3f441cd09c149cd58e8433156bb858d93efb3a3a
unknown <90293855+fishdestroyer@users.noreply.github.com>
2021-10-31 00:30:03 -0500

    map with pop up on hover

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZmlzaGRlc3Ryb3llciIsImEiOiJja3ZhaGIwam9hZGNmM
 //mapbox://styles/fishdestroyer/ckvaf22yi1dyv14teexn4pdq1/draft- draft of maps
 //mapbox://styles/fishdestroyer/ckvaf22yi1dyv14teexn4pdq1
 //below shows to be a const rather than a var- results did not work correctly
+
 var map = new mapboxgl.Map({
   container: 'map',
   
@@ -13,6 +14,86 @@ var map = new mapboxgl.Map({
 
 });
 
+
+map.on('load', () => {
+  map.addSource('places', {
+  'type': 'geojson',
+  'data': {
+  'type': 'FeatureCollection',
+  'features': [
+  {
+  'type': 'Feature',
+  'properties': {
+  'description':
+  '<strong>Veracruz All Natural</strong><p>All natural AND good taste</p>'
+  },
+  'geometry': {
+  'type': 'Point',
+  'coordinates': [-97.788, 30.231]
+  }
+  },
+  {
+  'type': 'Feature',
+  'properties': {
+  'description':
+  '<strong>Torchys Tacos</strong><p>Damn good tacos!</p>'
+  },
+  'geometry': {
+  'type': 'Point',
+  'coordinates': [-97.752, 30.245]
+  }
+  },
+  {
+  'type': 'Feature',
+  'properties': {
+  'description':
+  '<strong>TacoDeli</strong><p>Bienvenidos Yall</p>'
+  },
+  'geometry': {
+  'type': 'Point',
+  'coordinates': [-97.74014359, 30.31039002]
+}
+},
+]
+}
+});
+  
+  map.addLayer({
+  'id': 'places',
+  'type': 'circle',
+  'source': 'places',
+  'paint': {
+  'circle-color': '#4264fb',
+  'circle-radius': 6,
+  'circle-stroke-width': 2,
+  'circle-stroke-color': '#ffffff'
+  }
+  });
+   
+  const popup = new mapboxgl.Popup({
+  closeButton: false,
+  closeOnClick: false
+  });
+   
+  map.on('mouseenter', 'places', (e) => {
+  
+  map.getCanvas().style.cursor = 'pointer';
+   
+  const coordinates = e.features[0].geometry.coordinates.slice();
+  const description = e.features[0].properties.description;
+   
+  while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+  coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+  }
+   
+  popup.setLngLat(coordinates).setHTML(description).addTo(map);
+  });
+   
+  map.on('mouseleave', 'places', () => {
+  map.getCanvas().style.cursor = '';
+  popup.remove();
+  });
+  });
 
 
 // Add zoom and rotation controls to the map.
