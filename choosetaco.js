6ebed6728754c54a468ce0b17505c67096ccf82f
School <kaboy@icloud.com>
2021-11-02 20:43:16 -0500

    update choose taco feature

--- choosetaco.js
+++ choosetaco.js
@@ -1,29 +1,36 @@
-var htmlElem = document.querySelector('html');
-var pElem = document.querySelector('p');
-var imgElem = document.querySelector('img');
-
-var tacoForm = document.getElementById('image');
-
-if (!localStorage.getItem('bgcolor')) {
-  populateStorage();
-} else {
-  setStyles();
+if (localStorage.getItem('myFavoriteTaco') === null) {
+  localStorage.setItem('myFavoriteTaco', "No taco selected");
 }
+else {
+  if (localStorage.getItem('myFavoriteTaco') === "Crossroads") {
+  } else if (localStorage.getItem('myFavoriteTaco') === "Baja Shrimp") {
+    document.write("Baja Shrimp");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Green Chile Pork") {
+    document.write("Green Chile Pork");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Trailer Park") {
+    document.write("Trailer Park");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Fried Avocado") {
+    document.write("Fried Avocado");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Cowboy") {
+    document.write("Cowboy");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Shrimposito") {
+    document.write("Shrimposito");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Puerco Verde") {
+    document.write("Puerco Verde");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Pollo Fantastico") {
+    document.write("Pollo Fantastico");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Freakin' Vegan") {
+    document.write("Freakin' Vegan");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Steak") {
+    document.write("Steak");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Fish Taco") {
+    document.write("Fish Taco");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Al Pastor") {
+    document.write("Al Pastor");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Chicken") {
+    document.write("Chicken");
+  } else if (localStorage.getItem('myFavoriteTaco') === "Migas") {
+    document.write("Migas");
 
-function populateStorage() {
-  localStorage.setItem('bgcolor', document.getElementById('bgcolor').value);
-  localStorage.setItem('font', document.getElementById('font').value);
-  localStorage.setItem('image', document.getElementById('image').value);
-
-  setStyles();
+  }
 }
-
-function setStyles() {
-  var currentTaco = localStorage.getItem('image');
-
-  document.getElementById('image').value = currentTaco;
-
-  imgElem.setAttribute('src', currentTaco);
-}
-
-tacoForm.onchange = populateStorage;
